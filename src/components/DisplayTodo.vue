c5921f19707239aa359acbb84950f2aaa9c8b25f
kankan <sunagakannta7@gmail.com>
2019-08-18 14:55:01 +0900

    fit:cliがやっと動いた

---
+++ src/components/DisplayTodo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+  import Todo from './todo.js'
+  export default {
+    name: 'Todo',
+    props: {
+      msg: String
+    }
+  }
+</script>
